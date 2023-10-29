63e08b45a17914c935b5cda8927f84b4ba269653
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-10-29 21:28:58 +0900

    displaying data in a list

--- Scrumdinger/ScrumdingerApp.swift
+++ Scrumdinger/ScrumdingerApp.swift
@@ -4,7 +4,7 @@ import SwiftUI
 struct ScrumdingerApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentViewMeeting()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }

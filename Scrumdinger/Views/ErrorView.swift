6c10e10e2604c9134fa165db4e68c5620a3c1c12
RADISH-miyazaki <y.hidaka.kobe@gmail.com>
2023-11-01 15:32:29 +0900

    restruct directory & handling errors

---
+++ Scrumdinger/Views/ErrorView.swift
@@ -0,0 +1,47 @@
+import SwiftUI
+
+struct ErrorView: View {
+    let errorWrapper: ErrorWrapper
+    @Environment(\.dismiss) private var dismiss
+    
+    var body: some View {
+        NavigationStack {
+            VStack {
+                Text("An error has occurred!")
+                    .font(.title)
+                    .padding(.bottom)
+                Text(errorWrapper.error.localizedDescription)
+                    .font(.headline)
+                Text(errorWrapper.guidance)
+                    .font(.caption)
+                    .padding(.top)
+                Spacer()
+            }
+            .padding()
+            .background(.ultraThinMaterial)
+            .cornerRadius(16.0)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Dismiss") {
+                        dismiss()
+                    }
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    enum SampleError: Error {
+        case errorRequired
+    }
+    
+    var wrapper: ErrorWrapper {
+        ErrorWrapper(
+            error: SampleError.errorRequired,
+            guidance: "You can safely ignore this error."
+        )
+    }
+    
+    return ErrorView(errorWrapper: wrapper)
+}
